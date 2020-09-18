eea8d0d45c6a46c6c0377a8841b2fb50ab8e989c
Josiah Dahl <josiah.dahl@gmail.com>
2020-09-17 21:14:40 -0700

    refactor: Migrate to Chakra UI

--- apps/discover-daily/src/app/pages/dashboard.tsx
+++ apps/discover-daily/src/app/pages/dashboard.tsx
@@ -2,37 +2,61 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { SimpleAlbum } from '@discover-daily/integrations/spotify';
 import { apiClient } from '../services/api-client';
-import { Box, Heading } from '@chakra-ui/core';
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Heading,
+  Image,
+  Link,
+  Text,
+} from '@chakra-ui/core';
 
 const Releases = (props: { releases: SimpleAlbum[] }) => (
-  <section className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-8 gap-1 md:gap-3">
+  <Grid
+    as="section"
+    templateColumns={{
+      base: 'repeat(2, 1fr)',
+      sm: 'repeat(3, 1fr)',
+      md: 'repeat(4, 1fr)',
+      lg: 'repeat(6, 1fr)',
+      xl: 'repeat(8, 1fr)',
+    }}
+    gap={{ base: 1, md: 3 }}
+  >
     {props.releases.map((release) => (
-      <article
-        className="border border-r shadow flex flex-col"
+      <Flex
+        as="article"
+        borderWidth={1}
+        borderRadius={3}
+        boxShadow="md"
+        direction="column"
         key={release.id}
       >
-        <img
-          src={release.images[0].url}
-          className="w-full"
-          alt={`${release.name} album art`}
-        />
-        <div className="p-2 flex flex-col justify-between flex-grow">
-          <div className="flex-grow mb-3">
-            <h2 className="md:text-lg xl:text-xl">{release.name}</h2>
-            <p className="text-sm">{release.artists[0].name}</p>
-          </div>
-        </div>
-        <a
+        <Image src={release.images[0].url} alt={`${release.name} album art`} />
+        <Flex p={2} direction="column" justify="between" flexGrow={1}>
+          <Box flexGrow={1} mb={3}>
+            <Heading fontSize={{ md: 'sm', xl: 'md' }}>{release.name}</Heading>
+            <Text fontSize="sm">{release.artists[0].name}</Text>
+          </Box>
+        </Flex>
+        <Link
           href={release.external_urls.spotify}
           target="_blank"
           rel="noreferrer"
-          className="p-2 bg-teal-800 mx-auto block w-full text-center text-white uppercase"
+          bg="teal.800"
+          p={2}
+          display="block"
+          textAlign="center"
+          color="white"
+          textTransform="uppercase"
         >
           Listen
-        </a>
-      </article>
+        </Link>
+      </Flex>
     ))}
-  </section>
+  </Grid>
 );
 
 export const Dashboard = () => {
@@ -51,8 +75,12 @@ export const Dashboard = () => {
   }, []);
 
   return (
-    <Box as="main">
-      <Heading as="h1">Discover Daily</Heading>
+    <Box as="main" p={2}>
+      <Flex mb={4} justify="space-between">
+        <Heading as="h1">Discover Daily</Heading>
+        <Button variant="link">Logout</Button>
+      </Flex>
+
       {newReleases.length === 0 ? (
         <p>Fetching new releases...</p>
       ) : (
