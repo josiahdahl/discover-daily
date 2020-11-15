4fbfdbf1d8a09bc5caee592fe691a06c920ebe9f
Josiah Dahl <josiah.dahl@gmail.com>
2020-11-15 11:09:27 -0800

    fix: lint errors

--- apps/discover-daily/src/app/pages/landing.tsx
+++ apps/discover-daily/src/app/pages/landing.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, Heading, Image, Stack, Text } from '@chakra-ui/core';
+import { Button, Image, Stack, Text } from '@chakra-ui/core';
 import { apiClient } from '../services/api-client';
 import { CenteredFullPage } from '../components/Layout';
 
